2c7e2da2b46af3fa43a287eb7e20981c2d2821e1
Hannah Jordan <hjorda0126@aol.com>
2015-07-09 00:59:28 -0400

    pages almost complete, all have info on them and route correctly

--- app/router.js
+++ app/router.js
@@ -6,6 +6,66 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('questions', function() {
+    this.route('1');
+    this.route('2');
+    this.route('3');
+    this.route('4');
+    this.route('5');
+    this.route('6');
+    this.route('7');
+    this.route('8');
+    this.route('9');
+    this.route('10');
+    this.route('11');
+    this.route('12');
+  });
+
+  this.route('correct', function() {
+    this.route('1');
+    this.route('2');
+    this.route('3');
+    this.route('4');
+    this.route('5');
+    this.route('6');
+    this.route('7');
+    this.route('8');
+    this.route('9,');
+    this.route('10');
+    this.route('11');
+    this.route('12');
+  });
+
+  this.route('incorrect', function() {
+    this.route('1');
+    this.route('2');
+    this.route('3');
+    this.route('4');
+    this.route('5');
+    this.route('6');
+    this.route('7');
+    this.route('8');
+    this.route('9');
+    this.route('10');
+    this.route('11');
+  });
+
+  // 
+  // this.resource('base', {
+  //   path: '/'
+  // }, function() {
+  //
+  //   this.path('application');
+  //
+  //   // the rest of the app
+  // });
+  //
+  // this.resource('westEnd', {
+  //   path: '/questions/1'
+  // }, function() {
+  //   // admin part
+  // });
+
 });
 
 export default Router;
--- config/environment.js
+++ config/environment.js
@@ -16,7 +16,17 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+
+      contentSecurityPolicy: {
+    'default-src': "'none'",
+    'script-src': "'self'",
+    'font-src': "'self' fonts.gstatic.com",
+    'connect-src': "'self'",
+    'img-src': "'self'",
+    'media-src': "'self'"
+  },
     }
+
   };
 
   if (environment === 'development') {
